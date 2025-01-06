1d92d3e9b8d74fefebaacad33f8c7bcd8d3f55dd
qheog <qheogus55@naver.com>
2025-01-07 02:02:34 +0900

    feat: 댓글 수 api 작업
    실시간 count 쿼리 방식을 사용하게 되면 단점
    - COUNT 쿼리의 성능 이슈 (데이터가 많을 때)
    - 실시간 집계로 인한 DB 부하
    
    단점을 생각하여 댓글 수 테이블 생성

---
+++ comment-service/src/main/java/board/comment/entity/ArticleCommentCount.java
@@ -0,0 +1,26 @@
+package board.comment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArticleCommentCount {
+
+    @Id
+    private Long articleId;
+    private Long commentCount;
+
+    public static ArticleCommentCount init(Long articleId,Long commentCount){
+        ArticleCommentCount articleCommentCount = new ArticleCommentCount();
+        articleCommentCount.articleId = articleId;
+        articleCommentCount.commentCount = commentCount;
+        return articleCommentCount;
+    }
+}
