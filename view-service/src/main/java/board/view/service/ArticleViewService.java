20c29278b26daba93a49942cc12525a8609751b8
qheog <qheogus55@naver.com>
2025-01-07 03:18:45 +0900

    feat: 조회수 어뷰징 방지 정책 구현
    - 각 사용자는 게시글 1개당 10분에 1번 조회수 증가
    - 10분에 2회 이상 조회할 시 어뷰징으로 판단하여 집계 x
    - 게시글 조회수는 트래픽이 많을것을 예상하여 ttl도 지원하는 Redis를 사용하여 구현.

--- view-service/src/main/java/board/view/service/ArticleViewService.java
+++ view-service/src/main/java/board/view/service/ArticleViewService.java
@@ -1,8 +1,12 @@
 package board.view.service;
 
 import board.view.repository.ArticleViewCountRedisRepository;
+import board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.IllegalFormatCodePointException;
 
 @Service
 @RequiredArgsConstructor
@@ -10,14 +14,20 @@ public class ArticleViewService {
 
     private final ArticleViewCountRedisRepository articleViewCountRedisRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
 
     private static final int BACK_UP_BACK_SIZE = 100;
+    private static final Duration ABUSING_TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId,userId,ABUSING_TTL)){
+            return articleViewCountRedisRepository.read(articleId);
+        }
+
         Long count = articleViewCountRedisRepository.increase(articleId);
 
         if (count % BACK_UP_BACK_SIZE == 0) {
-            articleViewCountBackUpProcessor.backUp(articleId,count);
+            articleViewCountBackUpProcessor.backUp(articleId, count);
         }
         return count;
     }
